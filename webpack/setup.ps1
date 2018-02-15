4dceb9c4bd0a3051ba1aff2f7c590227312f08d2
nate <nate@cellepathy.com>
2018-02-14 18:40:53 -0800

    added missing dependency for jquery

--- webpack/setup.ps1
+++ webpack/setup.ps1
@@ -4,4 +4,5 @@ npm i babel-loader babel-core babel-preset-env babel-preset-react --save-dev
 npm i react react-dom --save-dev
 npm i prop-types --save-dev
 npm i html-webpack-plugin html-loader --save-dev
+npm i jquery
 npm i babel-plugin-recharts recharts
